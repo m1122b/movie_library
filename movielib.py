60e81869b19b4e2c381d62b0219e67d8b0e9da73
Miroslaw Biernacki <mb100773@gmail.com>
2022-02-09 20:57:50 +0100

    Change movielib.py

--- movielib.py
+++ movielib.py
@@ -3,11 +3,15 @@ from string import digits
 from faker import Faker 
 fake = Faker('pl_PL')
 
+"""
 print(fake.sentence(nb_words=4))
 print(fake.random_number(digits=2))
 print(fake.year())
 print(fake.word())
+print(fake.random_int(min=0, max=20, step=1))
+"""
 
+#Klasy!!!!!!!!!!!!!!!!!!!!!!!!
 
 class Movie:
     """
@@ -51,11 +55,11 @@ class TvSeries(Movie):
 
 
 
+#FUNKCJE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
-def create_movies_series( number_of: int = 6) -> list:
+def create_movies_series(number_of: int) -> list:
     """"   
-    number_of_cards (integer) - from 1 to .......
+    number_of (integer) - from 1 to ....... -> number of movies and TVseries
 
     """
     movies_series = []
@@ -86,6 +90,7 @@ def create_movies_series( number_of: int = 6) -> list:
 def get_movies(mylist: list):
     """
     wyszukuje tylko filmy
+    Znacznik categories = 0 oznacza filmy
 
     """
     result_list = []
@@ -99,7 +104,8 @@ def get_movies(mylist: list):
 
 def get_series(mylist: list):
     """
-    wyszukuje tylko filmy
+    wyszukuje tylko seriale
+    Znacznik categories = 1 oznacza seriale
 
     """
     result_list = []
@@ -111,27 +117,98 @@ def get_series(mylist: list):
     return result_list
 
 
+def search(mylist: list, mytitle: str):
+    """
+    mylist - lista filmów i seriali \n
+    mytitle - szukany tytuł
+
+    """
+
+    for i in range(len(mylist)):
+        if mylist[i].title.find(mytitle) > 0:
+            print(mylist[i])
+        
+
+def decorator(func):
+    def wrapper(mylist):
+        for _ in range(10):
+            func(mylist)
+    return wrapper
 
 
 
-all_list = create_movies_series()
+@decorator
+def generate_views(mylist):
+    """
+    mylist - cała lista filmów i seriali
 
+    """
+    n = len(mylist)-1
+    
+    random_number = fake.random_int(0, n, 1)
+    mylist[random_number].number_of_playes = fake.random_int(0, 100, 1)
+    print(mylist[random_number])
+    print(mylist[random_number].number_of_playes)
+
+
+def top_titles(mylist: list, number: int = 3) -> list:
+    """
+    mylist -> cała lista filmów i seriali \n
+    number -> wybrana ilość tytułów od góry
+
+    """
+    by_titles = sorted(mylist, key=lambda movie: movie.number_of_playes, reverse=True)
+    result = []
+    for i in range(number):
+        result.append(by_titles[i])
+    
+    return result
+
+
+
+#MAIN PROGRAM!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+print("\n")
+
+all_list = create_movies_series(7)
+
+print("all_list:")
 for i in range(len(all_list)):
     print(all_list[i])
 
 print("\n")
 
 only_movies = get_movies(all_list)
-
+print("only_movies:")
 for i in range(len(only_movies)):
     print(only_movies[i])
 
 print("\n")
-
+print("only series:")
 only_series = get_series(all_list)
 
 for i in range(len(only_series)):
     print(only_series[i])
+
+print("\n")
+
+
+print("Szukane tytuły: ")
+search(all_list, 'am')
+
+print("\n")
+
+print("Przypadkowy tytuł:")
+generate_views(all_list)
+
+print("\n")
+
+
+print("Najpopularniejsze tytuły:")
+ab = 4
+for i in range(ab):
+    print(top_titles(all_list, ab)[i])
 
 print("\n")
 
